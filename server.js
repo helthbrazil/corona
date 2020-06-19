8633c62908a205c21e3c4a3cd06c9b85e0b9cb73
Hebert Ferreira <hebertalvesferreira@gmail.com>
2020-06-19 13:40:03 -0300

    correçao path

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist/corona'));
+
+app.get('*', function(req, res){
+    res.sendfile(path.join(__dirname+ '/dist/corona/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
